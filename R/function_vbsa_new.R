1f2c899d7fb7f4feb8e9a2fa401f406150606d86
Tobias Pilz <tpilz@uni-potsdam.de>
2018-10-05 17:33:12 +0200

    ENH: Added option for re-start of runs that broke due to a runtime error

--- R/function_vbsa_new.R
+++ R/function_vbsa_new.R
@@ -64,6 +64,10 @@
 #' \code{fn}; \code{vbsa_backup3.RData} after checking (and possibliy alterating if
 #' \code{na.handle = TRUE}) the output of \code{fn}; \code{vbsa_backup4.RData} after
 #' calculation of sensitivity indices, before compiling the output. Default: \code{FALSE}.
+#' 
+#' @param restart Character list of two elements: 'backup': path to backup files
+#' created by a previous run with \code{debug = TRUE}; 'log': path to logfiles
+#' containing parameter values and \code{fn} outputs from a previous run (dee details).
 #'
 #' @return For each output of \code{fn} an optionally named (sub-)list with the following elements:
 #'
@@ -128,6 +132,20 @@
 #' Note that NA values might cause trouble with some output metrics! Unless it is a problem
 #' with your model, you could try to increase \code{N} and/or reduce subsampling intervalls
 #' (i.e. increase N for each subsample) to avoid NAs.
+#' 
+#' @details 
+#' There is an option to re-start previous function executions that terminated with
+#' an error. To do so, use argument \code{restart}. However, currently this is still
+#' rather unflexible. There must be logfiles in directory \code{restart$log} detectable
+#' via regexp '^log[-_0-9a-zA-Z]*.dat$'. Each file has to correspond to a single
+#' evaluation of \code{fn}. It must be a table with the elements 'group' (requires
+#' entries 'pars' and 'output'), 'variable' (the parameter and output element names),
+#' and 'value' (the actual parameter / output value). Internally, the parameter
+#' realisations found in the logfiles will be compared to the parameter matrices stored
+#' in debug file 'vbsa_backup1.RData'. Missing evaluations of \code{fn} will then be
+#' conducted and sensitivity indices be calculated. If \code{restart$log = NULL},
+#' only the indices will be calculated based on information stored in 'vbsa_backup2.RData',
+#' which will be required.
 #'
 #' @references Theory:
 #'
@@ -182,7 +200,8 @@ vbsa <- function(
   verbose= TRUE,                  # logical, indicating if progress messages have to be printed
   full.output=FALSE,
   na.handle = "stop",
-  debug = FALSE
+  debug = FALSE,
+  restart = NULL
 ) {
 
   #### INPUT CHECKING AND INITIALISATIONS ####
@@ -191,144 +210,297 @@ vbsa <- function(
     message("[ START SENSITIVITY ANALYSIS! ]")
   }
 
-  # Assigning the objective function
-  fn <- match.fun(fn)
-
-  # checking length of 'lower' and 'upper'
-  if (length(lower) != length(upper) )
-    stop( paste( "Invalid argument: 'length(lower) != length(upper) (", length(lower), "!=", length(upper), ")'", sep="" ) )
-
-  rtype      <- match.arg(rtype, c("quasirandom", "lhs"))
-  scrambling <- match.arg(scrambling, c("Owen","Faure-Tezuka","Owen+Faure-Tezuka","none"))
-  scrambling <- pmatch(scrambling, c("Owen","Faure-Tezuka","Owen+Faure-Tezuka","none"))
-  if (scrambling > 3) scrambling <- 0
-
-  # parallel version
-  if(ncores > 1)
-    registerDoMC(ncores)
-
-  # Checking that 'N' is integer
-  if ( trunc(N) != N ) stop( "Invalid argument: 'N' must be integer !" )
-
-  # check na.handle
-  if(!any(na.handle == c("stop", "remove"))) stop("Invalid argument: 'na.handle' must be one of {'stop', 'remove'}!")
-
-  # checking 'X.Boundaries'
-  if ( (lower[1L] == -Inf) || (upper[1L] == Inf) ) {
-    #if (any(upper==Inf | lower==-Inf))
-    stop( "Invalid argument: 'lower' and 'upper' boundaries must be finite !!'" )
-  } else X.Boundaries <- cbind(lower, upper)
-
-  # Computing 'K', the Dimension of the Solution Space
-  K <- nrow(X.Boundaries)
-
-  # Meaningful name of each one of the parameters
-  if (is.null(rownames(X.Boundaries))) {
-    param.IDs <- paste("Param", 1:K, sep="")
-  } else param.IDs <- rownames(X.Boundaries)
-
-  if (rtype=="quasirandom") {
-    # Backing up the original boundaries
-    lower.ini <- lower
-    upper.ini <- upper
-    X.Boundaries.ini <- X.Boundaries
-    LOWER.ini <- matrix( rep(lower.ini, N), nrow=N, byrow=TRUE)
-    UPPER.ini <- matrix( rep(upper.ini, N), nrow=N, byrow=TRUE)
-
-    # normalising
-    lower <- rep(0, K)
-    upper <- rep(1, K)
-    X.Boundaries <- cbind(lower, upper)
-    rownames(X.Boundaries) <- param.IDs
-  } # IF end
-
-  # check for sub-sampling
-  if(!is.null(subsamp)) {
-    if(!is.numeric(subsamp)) stop("Argument 'subsamp' must be of type numeric or NULL!")
-    if(any(subsamp == 1)) {
-      warning("Argument 'subsamp' should only contain values > 1. Values of 1 will be disregarded!")
-      subsamp <- subsamp[-which(subsamp == 1)]
-      if(length(subsamp) == 0) subsamp <- N
+  # check if this is a restart of a previous run
+  if(!is.null(restart)) {
+    
+    if(verbose) message("[ Re-start of a previous run ]")
+    
+    # parallel version
+    if(ncores > 1)
+      registerDoMC(ncores)
+    
+    # load vbsa_backup1.RData
+    if(!file.exists(paste(restart$backup, "vbsa_backup1.RData", sep="/")))
+      stop("Could not find backup file 'vbsa_backup1.RData' which is required!")
+    
+    load(paste(restart$backup, "vbsa_backup1.RData", sep="/"))
+    
+    if(verbose) message("[ Loaded vbsa_backup1.RData ]")
+    
+  } else { # no restart
+    
+    # Assigning the objective function
+    fn <- match.fun(fn)
+  
+    # checking length of 'lower' and 'upper'
+    if (length(lower) != length(upper) )
+      stop( paste( "Invalid argument: 'length(lower) != length(upper) (", length(lower), "!=", length(upper), ")'", sep="" ) )
+  
+    rtype      <- match.arg(rtype, c("quasirandom", "lhs"))
+    scrambling <- match.arg(scrambling, c("Owen","Faure-Tezuka","Owen+Faure-Tezuka","none"))
+    scrambling <- pmatch(scrambling, c("Owen","Faure-Tezuka","Owen+Faure-Tezuka","none"))
+    if (scrambling > 3) scrambling <- 0
+  
+    # parallel version
+    if(ncores > 1)
+      registerDoMC(ncores)
+  
+    # Checking that 'N' is integer
+    if ( trunc(N) != N ) stop( "Invalid argument: 'N' must be integer !" )
+  
+    # check na.handle
+    if(!any(na.handle == c("stop", "remove"))) stop("Invalid argument: 'na.handle' must be one of {'stop', 'remove'}!")
+  
+    # checking 'X.Boundaries'
+    if ( (lower[1L] == -Inf) || (upper[1L] == Inf) ) {
+      #if (any(upper==Inf | lower==-Inf))
+      stop( "Invalid argument: 'lower' and 'upper' boundaries must be finite !!'" )
+    } else X.Boundaries <- cbind(lower, upper)
+  
+    # Computing 'K', the Dimension of the Solution Space
+    K <- nrow(X.Boundaries)
+  
+    # Meaningful name of each one of the parameters
+    if (is.null(rownames(X.Boundaries))) {
+      param.IDs <- paste("Param", 1:K, sep="")
+    } else param.IDs <- rownames(X.Boundaries)
+  
+    if (rtype=="quasirandom") {
+      # Backing up the original boundaries
+      lower.ini <- lower
+      upper.ini <- upper
+      X.Boundaries.ini <- X.Boundaries
+      LOWER.ini <- matrix( rep(lower.ini, N), nrow=N, byrow=TRUE)
+      UPPER.ini <- matrix( rep(upper.ini, N), nrow=N, byrow=TRUE)
+  
+      # normalising
+      lower <- rep(0, K)
+      upper <- rep(1, K)
+      X.Boundaries <- cbind(lower, upper)
+      rownames(X.Boundaries) <- param.IDs
+    } # IF end
+  
+    # check for sub-sampling
+    if(!is.null(subsamp)) {
+      if(!is.numeric(subsamp)) stop("Argument 'subsamp' must be of type numeric or NULL!")
+      if(any(subsamp == 1)) {
+        warning("Argument 'subsamp' should only contain values > 1. Values of 1 will be disregarded!")
+        subsamp <- subsamp[-which(subsamp == 1)]
+        if(length(subsamp) == 0) subsamp <- N
+      }
+      if(length(subsamp) == 1) {
+        subsamp <- seq(subsamp, N, by = subsamp)
+      }
+      if(max(subsamp) > N) stop("Maximum value of 'subsamp' must be <= N!")
+      if(max(subsamp) < N) subsamp <- c(subsamp, N)
+    } else {
+      subsamp <- N
     }
-    if(length(subsamp) == 1) {
-      subsamp <- seq(subsamp, N, by = subsamp)
-    }
-    if(max(subsamp) > N) stop("Maximum value of 'subsamp' must be <= N!")
-    if(max(subsamp) < N) subsamp <- c(subsamp, N)
-  } else {
-    subsamp <- N
-  }
-
-  # Total Number of parameter sets to be used in the GSA
-  nparamsets  <- N*(K+2)
-
-  if(verbose) message("[ Number of Parameter Sets to be run ( N*(K+2) ): ", nparamsets, " ]")
-
-
-  #### MAIN BODY ####
-
-  #### Matrix sampling ####
-  if(verbose) message("[ Generate sampling matrices A, B, and Ab ]")
-
-  # Matrices A and B
-  if (rtype=="quasirandom") {
-    rs <- randtoolbox::sobol(n=N, dim=2*K, scrambling=scrambling)
-    A <- rs[, 1:K] * (UPPER.ini - LOWER.ini) + LOWER.ini
-    B <- rs[, (K+1):(2*K)] * (UPPER.ini - LOWER.ini) + LOWER.ini
-  } else if (rtype=="lhs") {
-    # tp corrected (rLHS which does not exist replaced by randomLHS)
-    A <- randomLHS(n=N, k = K)
-    A <- t(apply(A, 1, function(x) qunif(x, min = X.Boundaries[,1], max = X.Boundaries[,2])))
-    B <- randomLHS(n=N, k = K)
-    B <- t(apply(B, 1, function(x) qunif(x, min = X.Boundaries[,1], max = X.Boundaries[,2])))
-    # tp end
-  } # ELSE end
-
-  # Matrix Ab dim(N*K,K) by radial sampling
-  Ab <- A[rep(1:nrow(A), each = K),]
-  for(k in 1:K) Ab[seq(k, N*K, K), k] <- B[,k]
-
-  colnames(A) <- param.IDs
-  colnames(B) <- param.IDs
-  colnames(Ab) <- param.IDs
-
-  if(debug) save(list = ls(all.names = TRUE), file = "vbsa_backup1.RData")
+  
+    # Total Number of parameter sets to be used in the GSA
+    nparamsets  <- N*(K+2)
+  
+    if(verbose) message("[ Number of Parameter Sets to be run ( N*(K+2) ): ", nparamsets, " ]")
+  
+  
+    #### MAIN BODY ####
+  
+    #### Matrix sampling ####
+    if(verbose) message("[ Generate sampling matrices A, B, and Ab ]")
+  
+    # Matrices A and B
+    if (rtype=="quasirandom") {
+      rs <- randtoolbox::sobol(n=N, dim=2*K, scrambling=scrambling)
+      A <- rs[, 1:K] * (UPPER.ini - LOWER.ini) + LOWER.ini
+      B <- rs[, (K+1):(2*K)] * (UPPER.ini - LOWER.ini) + LOWER.ini
+    } else if (rtype=="lhs") {
+      # tp corrected (rLHS which does not exist replaced by randomLHS)
+      A <- randomLHS(n=N, k = K)
+      A <- t(apply(A, 1, function(x) qunif(x, min = X.Boundaries[,1], max = X.Boundaries[,2])))
+      B <- randomLHS(n=N, k = K)
+      B <- t(apply(B, 1, function(x) qunif(x, min = X.Boundaries[,1], max = X.Boundaries[,2])))
+      # tp end
+    } # ELSE end
+  
+    # Matrix Ab dim(N*K,K) by radial sampling
+    Ab <- A[rep(1:nrow(A), each = K),]
+    for(k in 1:K) Ab[seq(k, N*K, K), k] <- B[,k]
+  
+    colnames(A) <- param.IDs
+    colnames(B) <- param.IDs
+    colnames(Ab) <- param.IDs
+  
+    if(debug) save(list = ls(all.names = TRUE), file = "vbsa_backup1.RData")
+  
+  } # restart?
 
 
   #### Function evaluation at sample points ####
 
-  if(ncores > 1) {
-    if(verbose) message(paste("[ Evaluation of matrix A in parallel mode, involves", N, "calls of fn ]"))
-    yA <- foreach(j=1:nrow(A), .combine = "cbind", .errorhandling = "pass") %dopar% unlist(fn(A[j,], ...))
-    if(verbose) message(paste("[ Evaluation of matrix B in parallel mode, involves", N, "calls of fn ]"))
-    yB <- foreach(j=1:nrow(B), .combine = "cbind", .errorhandling = "pass") %dopar% unlist(fn(B[j,], ...))
-    if(verbose) message(paste("[ Evaluation of matrix Ab in parallel mode, involves", N*K, "calls of fn ]"))
-    yAb <- foreach(j=1:nrow(Ab), .combine = "cbind", .errorhandling = "pass") %dopar% unlist(fn(Ab[j,], ...))
-  } else {
-    if(verbose) message(paste("[ Evaluation of matrix A, involves", N, "calls of fn ]"))
-    yA <- apply(A, 1, function(x) unlist(fn(x, ...)))
-    if(verbose) message(paste("[ Evaluation of matrix B, involves", N, "calls of fn ]"))
-    yB <- apply(B, 1, function(x) unlist(fn(x, ...)))
-    if(verbose) message(paste("[ Evaluation of matrix Ab, involves", N*K, "calls of fn ]"))
-    yAb <- apply(Ab, 1, function(x) unlist(fn(x, ...)))
-  }
+  # if restart, check for vbsa_backup2.RData (in this case no fn evaluations are necessary)
+  do_eval <- T
+  if(!is.null(restart)) {
+    
+    # load vbsa_backup2.RData
+    if(is.null(restart$log) & file.exists(paste(restart$backup, "vbsa_backup2.RData", sep="/"))) {
+      load(paste(restart$backup, "vbsa_backup2.RData", sep="/"))
+      do_eval <- F
+      if(verbose) message("[ Found and loaded vbsa_backup2.RData ]")
+      
+    } else { # try to get results from runs so far
+      
+      # param matrices as tidy df
+      dat_pars <- map_dfr(list("A"=A, "B"=B, "Ab"=Ab), function(m) {
+        as_tibble(round(m, 4)) %>%
+          mutate(n = 1:n())
+      }, .id = "matrix")
+      
+      # get data from logfiles
+      logfiles <- dir(restart$log, pattern = "^log[-_0-9a-zA-Z]*.dat$", full.names = T)
+      if(length(logfiles) == 0) stop("Could not find logfiles!")
+      dat_log <- map_dfr(logfiles, function(f) {
+        dat_t <- read_tsv(f, col_types = "ccc")
+        if(nrow(dat_t) == 0) return(NULL)
+        dat_t %>%
+          filter(group %in% c("pars", "output")) %>%
+          mutate(value = as.numeric(value)) %>%
+          select(-group) %>%
+          spread(key = variable, value = value)
+      }, .id = "file")
+      
+      # identify rows (col 'n') of dat_pars from data in dat_log
+      dat_key <- left_join(dat_log, dat_pars, by = param.IDs)
+      
+      if(!all(dat_key$matrix %in% c("A", "B", "Ab")) || !all(dat_key$n %in% dat_pars$n))
+        stop("Could not merge log data with parameter matrices!")
+      
+      # get rows in A, B, Ab already processed and adapt matrices accordingly
+      n_A <- sort(unique(filter(dat_key, matrix == "A")$n))
+      A_save <- A
+      A <- A[-n_A,]
+      n_B <- sort(unique(filter(dat_key, matrix == "B")$n))
+      B_save <- B
+      B <- B[-n_B,]
+      n_Ab <- sort(unique(filter(dat_key, matrix == "Ab")$n))
+      Ab_save <- Ab
+      Ab <- Ab[-n_Ab,]
+      
+      # matrices/vectors of results
+      dat_res <- dat_key %>%
+        select(-file, -!!param.IDs) %>%
+        arrange(matrix, n)
+      
+      yA_t <- dat_res %>%
+        filter(matrix == "A") %>%
+        complete(n=1:nrow(A_save), matrix) %>%
+        select(-n,-matrix) %>%
+        as.matrix() %>% t()
+      yB_t <- dat_res %>%
+        filter(matrix == "B") %>%
+        complete(n=1:nrow(B_save), matrix) %>%
+        select(-n,-matrix) %>%
+        as.matrix() %>% t()
+      yAb_t <- dat_res %>%
+        filter(matrix == "Ab") %>%
+        complete(n=1:nrow(Ab_save), matrix) %>%
+        select(-n,-matrix) %>%
+        as.matrix() %>% t()
+      
+      if(verbose) message("[ Extracted data from log files ]")
+    } # get results from log data
+      
+  } # restart?
   
-  # if 'fn' returns single NAs, try to convert results to matrix
-  y.to.mat <- function(x) {
-    if(any(is.na(x))) {
-      x <- rep(NA, length(param.IDs))
-      names(x) <- param.IDs
+  # do fn evaluations?
+  if(do_eval) {
+    
+    if(ncores > 1) {
+      if(verbose) {
+        if(is.null(restart)) {
+          message(paste("[ Evaluation of matrix A in parallel mode, involves", N, "calls of fn ]"))
+        } else {
+          message(paste("[ Evaluation of matrix A in parallel mode,", nrow(A), "calls of fn left ]"))
+        }
+      }
+      yA <- foreach(j=1:nrow(A), .combine = "cbind", .errorhandling = "pass") %dopar% unlist(fn(A[j,], ...))
+      
+      if(verbose) {
+        if(is.null(restart)) {
+          message(paste("[ Evaluation of matrix B in parallel mode, involves", N, "calls of fn ]"))
+        } else {
+          message(paste("[ Evaluation of matrix B in parallel mode,", nrow(B), "calls of fn left ]"))
+        }
+      }
+      yB <- foreach(j=1:nrow(B), .combine = "cbind", .errorhandling = "pass") %dopar% unlist(fn(B[j,], ...))
+      
+      if(verbose) {
+        if(is.null(restart)) {
+          message(paste("[ Evaluation of matrix Ab in parallel mode, involves", N, "calls of fn ]"))
+        } else {
+          message(paste("[ Evaluation of matrix Ab in parallel mode,", nrow(Ab), "calls of fn left ]"))
+        }
+      }
+      yAb <- foreach(j=1:nrow(Ab), .combine = "cbind", .errorhandling = "pass") %dopar% unlist(fn(Ab[j,], ...))
+      
+    } else {
+      
+      if(verbose) {
+        if(is.null(restart)) {
+          message(paste("[ Evaluation of matrix A, involves", N, "calls of fn ]"))
+        } else {
+          message(paste("[ Evaluation of matrix A,", nrow(A), "calls of fn left ]"))
+        }
+      }
+      yA <- apply(A, 1, function(x) unlist(fn(x, ...)))
+      
+      if(verbose) {
+        if(is.null(restart)) {
+          message(paste("[ Evaluation of matrix B, involves", N, "calls of fn ]"))
+        } else {
+          message(paste("[ Evaluation of matrix B,", nrow(B), "calls of fn left ]"))
+        }
+      }
+      yB <- apply(B, 1, function(x) unlist(fn(x, ...)))
+      
+      if(verbose) {
+        if(is.null(restart)) {
+          message(paste("[ Evaluation of matrix Ab, involves", N, "calls of fn ]"))
+        } else {
+          message(paste("[ Evaluation of matrix Ab,", nrow(Ab), "calls of fn left ]"))
+        }
+      }
+      yAb <- apply(Ab, 1, function(x) unlist(fn(x, ...)))
     }
-    return(x)
-  }
-  if(any(is.na(yA), is.na(yB), is.na(yAb)) & any(is.list(yA), is.list(yB), is.list(yAb))) {
-    warning("Function 'fn' produced NAs! Try to calculate indices anyway but check the results!")
-    yA <- sapply(yA, y.to.mat)
-    yB <- sapply(yB, y.to.mat)
-    yAb <- sapply(yAb, y.to.mat)
+    
+    # if 'fn' returns single NAs, try to convert results to matrix
+    y.to.mat <- function(x) {
+      if(any(is.na(x))) {
+        x <- rep(NA, length(param.IDs))
+        names(x) <- param.IDs
+      }
+      return(x)
+    }
+    if(any(is.na(yA), is.na(yB), is.na(yAb)) & any(is.list(yA), is.list(yB), is.list(yAb))) {
+      warning("Function 'fn' produced NAs! Try to calculate indices anyway but check the results!")
+      yA <- sapply(yA, y.to.mat)
+      yB <- sapply(yB, y.to.mat)
+      yAb <- sapply(yAb, y.to.mat)
+    }
+  
+  } # evaluations of fn necessary?
+  
+  # merge with data from logfiles (if restart)
+  if(!is.null(restart) & do_eval) {
+    yA_t[,-n_A] <- yA
+    yA <- yA_t
+    yB_t[,-n_B] <- yB
+    yB <- yB_t
+    yAb_t[,-n_Ab] <- yAb
+    yAb <- yAb_t
+    rm(yA_t, yB_t, yAb_t)
   }
 
-  if(debug) save(list = ls(all.names = TRUE), file = "vbsa_backup2.RData")
+  if(debug & do_eval) save(list = ls(all.names = TRUE), file = "vbsa_backup2.RData")
 
   # use current environment within internal function eval_fn()
   environment(eval_fn) <- environment()
